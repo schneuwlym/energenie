7c10cb1043c953c7aafd356d6565dc84c171fb5a
Mathias Schneuwly <mathias@schneuwlys.ch>
2016-05-22 15:00:36 +0200

    - Added a timeout to the request - Do always a logout - Check whether login was successful

--- energenie.py
+++ energenie.py
@@ -13,28 +13,34 @@ import urllib2
 
 class Energenie(object):
 
-    def __init__(self, ip, password):
+    def __init__(self, ip, password, timeout=5):
         self._ip = ip
         self._password = password
+        self._timeout = timeout
 
     def _do_request(self, url, data=None):
         if data is not None:
             data = urllib.urlencode(data)
         req = urllib2.Request(url=url,
                               data=data)
-
-        f = urllib2.urlopen(req)
+        f = urllib2.urlopen(req, timeout=self._timeout)
         result = f.read()
         f.close()
         return result
 
     def login(self):
-        self._do_request(url='http://%s/login.html' % self._ip,
-                         data={'pw': self._password})
+        res = self._do_request(url='http://%s/login.html' % self._ip,
+                               data={'pw': self._password})
+        if res == '' or 'EnerGenie Web:' in res:
+            return False
+        return True
 
     def logout(self):
-        self._do_request(url='http://%s/login.html' % self._ip,
-                         data={'pw': ''})
+        res = self._do_request(url='http://%s/login.html' % self._ip,
+                               data={'pw': ''})
+        if 'EnerGenie Web:' in res:
+            return True
+        return False
 
     def get_socket_state(self, socket=None):
         if socket is not None and socket not in xrange(1, 5):
@@ -76,6 +82,8 @@ if __name__ == "__main__":
                         help='')
     parser.add_argument('--socket', '-s', type=int, dest='socket', choices=range(1, 5), default=None,
                         help='')
+    parser.add_argument('--timeout', '-t', type=int, dest='timeout', default=5,
+                        help='')
     group.add_argument('--enable', '-e', dest='state', action='store_true',
                         help='')
     group.add_argument('--disable', '-d', dest='state', action='store_false',
@@ -85,10 +93,14 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    energenie = Energenie(args.ip, args.password)
-    energenie.login()
-    if args.get:
-        print energenie.get_socket_state(args.socket)
-    else:
-        energenie.set_socket_state(args.socket, args.state)
-    energenie.logout()
+    energenie = Energenie(args.ip, args.password, args.timeout)
+    try:
+        if not energenie.login():
+            raise Exception('Could not login')
+        if args.get:
+            print energenie.get_socket_state(args.socket)
+        else:
+            energenie.set_socket_state(args.socket, args.state)
+    finally:
+        if not energenie.logout():
+            raise Exception('Could not logout')
